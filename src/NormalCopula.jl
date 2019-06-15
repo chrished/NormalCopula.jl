9c653c91f1f9ca40a5e64f9118b8fbdfa9818957
Christoph Lenovo <christophhedtrich@gmail.com>
2019-06-15 16:57:09 +0200

    add initial implementation of normal copula density

--- src/NormalCopula.jl
+++ src/NormalCopula.jl
@@ -1,5 +1,21 @@
 module NormalCopula
+    using Distributions
+    using LinearAlgebra
 
-greet() = print("Hello World!")
+    export normcopula_density
+
+    const stdnormal = Normal()
+    function normcopula_density(u, R)
+        pvec = similar(u)
+        for i = 1:length(u)
+            pvec[i] = quantile(stdnormal, u[i])
+        end
+        RmI = inv(R)
+        for i = 1:length(u)
+            RmI[i,i] -= 1.
+        end
+        return 1/sqrt(det(R)) * exp(-0.5 * dot(pvec, RmI*pvec))
+    end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using NormalCopula
 using Test
+using LinearAlgebra
 
 @testset "NormalCopula.jl" begin
-    # Write your own tests here.
+    ndim = 5
+    R = diagm(0=>ones(ndim))
+    us = [rand(ndim) for i = 1:10]
+    for u in us
+        @test 1.0 ≈ normcopula_density(u, R)
+    end
 end
