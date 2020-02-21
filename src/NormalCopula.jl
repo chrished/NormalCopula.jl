67313ee0bf8484e20930be535b430efd5d8886c3
Christoph Hedtrich <chrhe280@user.uu.se>
2020-02-21 14:54:22 +0100

    add log copula

--- src/NormalCopula.jl
+++ src/NormalCopula.jl
@@ -2,7 +2,7 @@ module NormalCopula
     using Distributions
     using LinearAlgebra
 
-    export normcopula_density
+    export normcopula_density, normcopula_logdensity
 
     const stdnormal = Normal()
     function normcopula_density(u, R)
@@ -15,6 +15,18 @@ module NormalCopula
             RmI[i,i] -= 1.
         end
         return 1/sqrt(det(R)) * exp(-0.5 * dot(pvec, RmI*pvec))
+    end
+
+    function normcopula_logdensity(u, R)
+        pvec = similar(u)
+        for i = 1:length(u)
+            pvec[i] = quantile(stdnormal, u[i])
+        end
+        RmI = inv(R)
+        for i = 1:length(u)
+            RmI[i,i] -= 1.
+        end
+        return -log(sqrt(det(R))) + -0.5 * dot(pvec, RmI*pvec)
     end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,13 @@ using LinearAlgebra
         @test 1.0 ≈ normcopula_density(u, R)
     end
 end
+
+
+@testset "NormalCopula.jl - logdensity" begin
+    ndim = 5
+    R = diagm(0=>ones(ndim))
+    us = [rand(ndim) for i = 1:10]
+    for u in us
+        @test 0.0 ≈ normcopula_logdensity(u, R)
+    end
+end
